4029b13af8ebdc8b2f40032bd58a8d2a8352ebf2
RUSHABH SONI <slayer.r1@gmail.com>
2021-11-26 14:28:40 +0530

    added if clause

--- src/Product.js
+++ src/Product.js
@@ -4,20 +4,24 @@ import './Product.css'
 function Product({item}) {
   
   return (
+
+    item ?
     <div className="product">
-      {/* <div className="product__info">
+       
+      <div className="product__info">
         <p>{item.title}</p>
         <p className="product__price"> 
         <small>$</small>     
         <strong>{item.price}</strong>
-        </p> */}
+        </p> 
         {/* <div className="product__rating">
 
         </div> */}
-    {/* </div>
+     </div>
         <img src={item.image} alt={item.info}/>
-        <button>Add to Basket</button> */}
-    </div>
+        <button>Add to Basket</button>
+      </div> 
+    : <p> no product</p>
   )
 }
 
